70b85d4de74b80f3e10f3a80b3615eb8dbbb644e
Sarathsathyan <sarathsatthyan98@gmail.com>
2020-04-21 12:07:11 +0530

    new

--- Admin/forms.py
+++ Admin/forms.py
@@ -1,5 +1,10 @@
 from django.contrib.auth.forms import AuthenticationForm
 from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.core.exceptions import ObjectDoesNotExist
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={'class':'uk-input'}))
+
+
