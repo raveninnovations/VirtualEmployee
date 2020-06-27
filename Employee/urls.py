1afad80f01fb5389ca696506902b9476145beaff
Sarathsathyan <sarathsathyan98@gmail.com>
2020-06-27 19:14:14 +0530

    reset password

--- Employee/urls.py
+++ Employee/urls.py
@@ -57,19 +57,19 @@ urlpatterns = [
 
 
     path('reset_password/',
-     auth_views.PasswordResetView.as_view(template_name='virtualmain_pages/password_reset.html'),
+     auth_views.PasswordResetView.as_view(template_name='password_reset/password_reset.html'),
      name="reset_password"),
 
     path('reset_password_sent/',
-        auth_views.PasswordResetDoneView.as_view(template_name='virtualmain_pages/password_reset_sent.html'),
+        auth_views.PasswordResetDoneView.as_view(template_name='password_reset/password_reset_sent.html'),
         name="password_reset_done"),
 
-    path('reset/<uidb64>/<token>/',
-     auth_views.PasswordResetConfirmView.as_view(template_name='virtualmain_pages/password_reset_form.html'),
+    path('reset_password_sent/<uidb64>/<token>/',
+     auth_views.PasswordResetConfirmView.as_view(template_name='password_reset/password_reset_confirm.html'),
      name="password_reset_confirm"),
 
     path('reset_password_complete/',
-        auth_views.PasswordResetCompleteView.as_view(template_name='virtualmain_pages/password_reset_done.html'),
+        auth_views.PasswordResetCompleteView.as_view(template_name='password_reset/password_reset_done.html'),
         name="password_reset_complete"),
 
 
